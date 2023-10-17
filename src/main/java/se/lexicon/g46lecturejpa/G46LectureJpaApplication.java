84ce8cc78ce76e5114defd346cfb135e2d58c3c6
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-10-17 09:35:58 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g46lecturejpa/G46LectureJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g46lecturejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G46LectureJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(G46LectureJpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/lexicon/g46lecturejpa/G46LectureJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g46lecturejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G46LectureJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
